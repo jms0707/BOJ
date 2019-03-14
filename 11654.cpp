8f67997e6fc172d5457623f1414f340ecbd9f3c3
제갈윤 <jegal-yun@jegal-yun-ui-MacBookPro.local>
2019-03-14 22:38:36 +0900

    아스키 코드

---
+++ 11654.cpp
@@ -0,0 +1,17 @@
+//
+//  11654.cpp
+//  BOJ
+//
+//  Created by 제갈윤 on 14/03/2019.
+//  Copyright © 2019 제갈윤. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char a;
+    cin >> a;
+    cout << (int) a << endl;
+}
