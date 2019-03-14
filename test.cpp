e59aa2a4cffc050087ddbee3aff326e3f4cfa1f5
제갈윤 <jegal-yun@jegal-yun-ui-MacBookPro.local>
2019-03-14 22:28:33 +0900

    test

---
+++ test.cpp
@@ -0,0 +1,15 @@
+//
+//  test.cpp
+//  BOJ
+//
+//  Created by 제갈윤 on 14/03/2019.
+//  Copyright © 2019 제갈윤. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello world" << endl;
+}
